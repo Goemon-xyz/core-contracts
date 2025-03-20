9fe23f95f8d4d4cde0a9f716bb932652c155d920
Thileepan I <112769282+0xthi@users.noreply.github.com>
2025-03-20 12:53:03 +0530

    feat(batch-event): added batch intent function and upgraded contract

--- script/UpgradeUserManager.s.sol
+++ script/UpgradeUserManager.s.sol
@@ -7,9 +7,9 @@ import "../src/UserManager.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract UpgradeUserManager is Script {
-    // Replace with your existing proxy address
-    address public constant EXISTING_PROXY_ADDRESS = 0x01D75243e0f7d3145E985acC1D4007968A45B08e;
-    // New implementation contract address = 0xbCCc65cB4e2827a514CA6f4993FF0765345b3e4D
+
+    address public constant EXISTING_PROXY_ADDRESS = 0x46a3A1c66DcaEFE42045d3D16D391689b9F68812; //testnet sepolia
+    // address public constant EXISTING_PROXY_ADDRESS = 0xb7A73b686578a2994b65a57023784Be1304cEAd1; //mainnet ethereum
     
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
